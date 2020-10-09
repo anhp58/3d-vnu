97f78e19c38302792a9d86369ada3b9580c663ad
anhp58 <anhphan.uet@gmail.com>
2020-10-09 10:14:36 +0700

    update cesium 1.74 particle rain

--- Apps/Sandcastle/gallery/script/my_script.js
+++ Apps/Sandcastle/gallery/script/my_script.js
@@ -9,8 +9,10 @@ var MAX_RAINDROP_PER_TICK = 50;
 var TOTAL_RAIN_DROP = 3000;
 
 function startup(Cesium) {
-    console.log("startUp");
-    // Cesium.BingMapsApi.defaultKey = "zzWjk21MKE6PA3dnArsG~mXUlKXIqFe8nByQlxoCiPw~AkVQrb4F729BlIgUx5FmvX4VSpoomW7YLR5MU2pPwIVoGbXpcREF8OXD12gAcZNC";
+
+    // test sync two view
+
+
     Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDc0NTFhOC0zZjMxLTQ5NzctYjJmMy0yNzM0YTRhNDg3MmUiLCJpZCI6NzU4LCJpYXQiOjE2MDIxMzI1Mzd9.cLlRg9IJeBwtZUcG5qnBGKH0wM9cax6ArZrTvcNMMjk"
     // cesium viewer define
     var viewer = new Cesium.Viewer('cesiumContainer', {
@@ -531,6 +533,93 @@ function startup(Cesium) {
         return weather.toLowerCase().indexOf("storm") >= 0;
     }
     viewer.scene.globe.enableLighting = true;
+
+    var rainParticleSize = 15.0;
+    var rainRadius = 100000.0;
+    var rainImageSize = new Cesium.Cartesian2(
+      rainParticleSize,
+      rainParticleSize * 2.0
+    );
+
+    var resetCameraFunction = function () {
+      scene.camera.setView({
+        destination: new Cesium.Cartesian3.fromDegrees(105.782284, 21.039170, 100),
+        orientation: {
+          heading: 4.731089976107251,
+          pitch: -0.32003481981370063,
+        },
+      });
+    };
+    resetCameraFunction();
+    Sandcastle.addToolbarButton("Reset Camera", resetCameraFunction);
+    var rainSystem;
+
+    var rainGravityScratch = new Cesium.Cartesian3();
+    var rainUpdate = function (particle, dt) {
+      rainGravityScratch = Cesium.Cartesian3.normalize(
+        particle.position,
+        rainGravityScratch
+      );
+      rainGravityScratch = Cesium.Cartesian3.multiplyByScalar(
+        rainGravityScratch,
+        -1050.0,
+        rainGravityScratch
+      );
+
+      particle.position = Cesium.Cartesian3.add(
+        particle.position,
+        rainGravityScratch,
+        particle.position
+      );
+
+      var distance = Cesium.Cartesian3.distance(
+        scene.camera.position,
+        particle.position
+      );
+      if (distance > rainRadius) {
+        particle.endColor.alpha = 0.0;
+      } else {
+        particle.endColor.alpha =
+          rainSystem.endColor.alpha / (distance / rainRadius + 0.1);
+      }
+    };
+
+    rainSystem = new Cesium.ParticleSystem({
+      modelMatrix: new Cesium.Matrix4.fromTranslation(
+        scene.camera.position
+      ),
+      speed: -1.0,
+      lifetime: 15.0,
+      emitter: new Cesium.SphereEmitter(rainRadius),
+      startScale: 1.0,
+      endScale: 0.0,
+      image: "../../SampleData/circular_particle.png",
+      emissionRate: 9000.0,
+      startColor: new Cesium.Color(0.27, 0.5, 0.7, 0.0),
+      endColor: new Cesium.Color(0.27, 0.5, 0.7, 0.98),
+      imageSize: rainImageSize,
+      updateCallback: rainUpdate,
+    });
+    scene.primitives.add(rainSystem);
+
+    // drop down
+    var options = [
+      {
+        text: "Rain",
+        onselect: function () {
+          rainSystem.show = true;
+
+          scene.skyAtmosphere.hueShift = -0.97;
+          scene.skyAtmosphere.saturationShift = 0.25;
+          scene.skyAtmosphere.brightnessShift = -0.4;
+
+          scene.fog.density = 0.00025;
+          scene.fog.minimumBrightness = 0.01;
+        },
+      },
+    ];
+    Sandcastle.addToolbarMenu(options);
+
 
     Sandcastle.finishedLoading();
 }
